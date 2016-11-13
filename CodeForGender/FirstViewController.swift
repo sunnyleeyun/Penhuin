ab466b50a6e12b74163e8564bc9eac2f6de00e80
Kenny Lee <lekeny@gmail.com>
2016-11-13 14:24:20 +0800

    Initial Commit

---
+++ CodeForGender/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CodeForGender
+//
+//  Created by Daphne Jiang on 13/11/2016.
+//  Copyright © 2016 M.A.D. Crew. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
